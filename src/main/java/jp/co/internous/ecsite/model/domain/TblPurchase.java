7cb909f3a9793c182835010eaab9035c5217d957
オガタユリエ <mumurisu@gmail.com>
2023-09-29 17:11:21 +0900

    Lesson_ECsite

---
+++ src/main/java/jp/co/internous/ecsite/model/domain/TblPurchase.java
@@ -0,0 +1,62 @@
+package jp.co.internous.ecsite.model.domain;
+
+import java.sql.Timestamp;
+
+public class TblPurchase {
+	private int id;
+	private int userId;
+	private int goodsId;
+	private String goodsName;
+	private int itemCount;
+	private int total;
+	private Timestamp createAt;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public int getGoodsId() {
+		return goodsId;
+	}
+	public void setGoodsId(int goodsId) {
+		this.goodsId = goodsId;
+	}
+	
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	
+	public int getItemCount() {
+		return itemCount;
+	}
+	public void setItemCount(int itemCount) {
+		this.itemCount = itemCount;
+	}
+	
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	
+	public Timestamp getCreatedAt() {
+		return createAt;
+	}
+	public void setCreatedAt(Timestamp createAt) {
+		this.createAt = createAt;
+	}
+}
